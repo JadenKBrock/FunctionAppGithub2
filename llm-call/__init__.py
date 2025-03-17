2bb5aef1f5c7dc299ae6e6d24fcef26433fcf7e0
JadenKBrock <jk21pf@brocku.ca>
2025-03-17 14:03:22 -0400

    updated

--- llm-call/__init__.py
+++ llm-call/__init__.py
@@ -10,12 +10,9 @@ from azure.ai.inference.models import SystemMessage, UserMessage
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
 AZURE_ENDPOINT = "https://DeepSeek-R1-qfags.eastus2.models.ai.azure.com"
 AZURE_KEY = "5G4Mb2HfmaFkeKgnuYFnik2CnutqWlqj"
 
-@app.route(route="llm-call", methods=["POST"])
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
